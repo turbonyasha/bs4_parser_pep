420cbf18729413f58e5ab95f34224aef0eba97fd
Jane <mimimeta@bk.ru>
2025-01-18 15:47:14 +0400

    setup

---
+++ src/configs.py
@@ -0,0 +1,47 @@
+import argparse
+import logging
+from logging.handlers import RotatingFileHandler
+
+from constants import BASE_DIR
+
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+
+
+def configure_argument_parser(availible_modes):
+    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser.add_argument(
+        'mode',
+        choices=availible_modes,
+        help='Режим работы парсера'
+    )
+    parser.add_argument(
+        '-c',
+        '--clear-cache',
+        action='store_true',
+        help='Очистка кеша'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        choices=('pretty', 'file'),
+        help='Дополнительные способы вывода данных'
+    )
+    return parser
+
+
+def configure_logging():
+    log_dir = BASE_DIR / 'logs'
+    log_dir.mkdir(exist_ok=True)
+    log_file = log_dir / 'parser.log'
+    rotating_handler = RotatingFileHandler(
+        log_file, maxBytes=10 ** 6, backupCount=5, encoding='utf-8'
+    )
+    logging.basicConfig(
+        datefmt=DT_FORMAT,
+        format=LOG_FORMAT,
+        # Уровень записи логов.
+        level=logging.INFO,
+        # Вывод логов в терминал.
+        handlers=(rotating_handler, logging.StreamHandler())
+    )
